d911518991e682aebd65c14d69050347bc5c98a7
Federico Raimondo <federico.raimondo@claro.com.ar>
2020-10-20 16:40:57 -0300

    desktop

--- Javascript/app.js
+++ Javascript/app.js
@@ -31,7 +31,7 @@ var modo = document.getElementById("mode");
 
 function darkmode() {
     var cuerpo = document.body  //body
-    
+
     cuerpo.classList.toggle('darkmodemobile');          // toggle clase fondo gris
 
     if (modo.innerHTML == "Nocturno") {
@@ -40,24 +40,97 @@ function darkmode() {
     else {
         modo.innerHTML = "Nocturno";
     }
-    
+
     if (theme.getAttribute("href") == "Style/lightmode/lightstyle.css") {
         // ... then switch it to "dark-theme.css"
         theme.href = "Style/darkmode/darkstyle.css";
-        var logomovil = document.getElementById("logomobile").src="images/logo-mobile-modo-noct.svg";
-        var logo = document.getElementById("logo").src="images/Logo-modo-noc.svg";
-      // Otherwise...
-      } else {
+        var logomovil = document.getElementById("logomobile").src = "images/logo-mobile-modo-noct.svg";
+        var logo = document.getElementById("logo").src = "images/Logo-modo-noc.svg";
+        hoverRedesNoc();
+       
+        
+        // Otherwise...
+    } else {
         // ... switch it to "light-theme.css"
         theme.href = "Style/lightmode/lightstyle.css";
-        var logomovil = document.getElementById("logomobile").src="images/logo-mobile.svg";
-        var logo = document.getElementById("logo").src="images/logo-desktop.svg";
-      }
+        var logomovil = document.getElementById("logomobile").src = "images/logo-mobile.svg";
+        var logo = document.getElementById("logo").src = "images/logo-desktop.svg";
+    }
 
 
 }
-//-------------------------------------FULLSCREEN IMAGE----------------------------------------------------------
+//------------------------------------------------------HOVER MENU Y BOTON MAS----------------------------------------------------------
 
+var menu = document.getElementById("menu");
+menu.addEventListener("mouseover", function (event) {
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+    // reset the color after a short delay
+});
+menu.addEventListener("mouseout", function (event) {
+    event.target.style.color = "purple";
+});
 
+var botonagregar = document.getElementById("add");
+botonagregar.addEventListener("mouseover", function () {
+    botonagregar.classList.add("add-hover");
+})
+botonagregar.addEventListener("mouseout", function () {
+    botonagregar.classList.remove("add-hover");
+})
+botonagregar.addEventListener("click", function () {
+    botonagregar.classList.toggle("add-click");
+})
 
+//-----------------------------------------------------HOVER FOOTER-----------------------------------------------------------------------
 
+//-----------------------------------------------------Icono Facebook---------------------------------------------------------------------
+var facebook = document.getElementById("facebook");
+
+facebook.addEventListener("mouseover", function () {
+    facebook.src = "images/icon_facebook_hover.svg";
+});
+facebook.addEventListener("mouseout", function () {
+    facebook.src = "images/icon_facebook.svg";
+});
+//----------------------------------------------------Icono Twitter---------------------------------------------------------------------
+var twitter = document.getElementById("twitter");
+
+twitter.addEventListener("mouseover", function () {
+    twitter.src = "images/icon-twitter-hover.svg";
+});
+twitter.addEventListener("mouseout", function () {
+    twitter.src = "images/icon-twitter.svg";
+});
+//----------------------------------------------------Icono Instagram---------------------------------------------------------------------
+var insta = document.getElementById("instagram");
+
+insta.addEventListener("mouseover", function () {
+    insta.src = "images/icon_instagram-hover.svg";
+});
+insta.addEventListener("mouseout", function () {
+    insta.src = "images/icon_instagram.svg";
+});
+
+//---------------------------------------------------Modo Nocturno-------------------------------------------
+function hoverRedesNoc() {
+    facebook.addEventListener("mouseover", function () {
+        facebook.src = "images/icon_facebook_noc.svg";
+    });
+    facebook.addEventListener("mouseout", function () {
+        facebook.src = "images/icon_facebook.svg";
+    });
+
+    twitter.addEventListener("mouseover", function () {
+        twitter.src = "images/icon_twitter_noc.svg";
+    });
+    twitter.addEventListener("mouseout", function () {
+        twitter.src = "images/icon-twitter.svg";
+    });
+    insta.addEventListener("mouseover", function () {
+        insta.src = "images/icon_instagram_noc.svg";
+    });
+    insta.addEventListener("mouseout", function () {
+        insta.src = "images/icon_instagram.svg";
+    });
+}
