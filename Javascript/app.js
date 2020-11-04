2c80b0f31cb91bf51ffb223fb6e790b5d8982c3f
Federico Raimondo <federico.raimondo@claro.com.ar>
2020-11-04 17:04:13 -0300

    apis connection

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,5 +1,5 @@
 //--------------------------------------------- MENU HAMBURGUESA -----------------------------------------------------------------
-
+const apiKey = "Uc1F9kfal68vQWzdRi60gLKD2i59hyw0"
 var navbar = document.querySelector(".navbar")
 var ham = document.querySelector(".ham")
 
@@ -130,7 +130,7 @@ insta.addEventListener("mouseout", function () {
     insta.src = "images/icon_instagram.svg";
 });
 }
-//---------------------------------------------------Modo Nocturno-------------------------------------------
+//---------------------------------------------------Modo Nocturno-------------------------------------------------------------------------
 function hoverRedesNoc() {
     facebook.addEventListener("mouseover", function () {
         facebook.src = "images/icon_facebook_noc.svg";
@@ -153,3 +153,81 @@ function hoverRedesNoc() {
         insta.src = "images/icon_instagram.svg";
     });
 }
+
+
+//-----------------------------------------------------API CONNECTION TRENDIGS----------------------------------------------------------------------
+function trendigs () {
+var urlTrendings = `https://api.giphy.com/v1/trending/searches?api_key=${apiKey}`;
+fetch (urlTrendings)
+.then (r => r.json())
+.then ((rsp) => {
+    console.log(rsp);
+     var contTrendings = document.getElementById("TT");
+     for (i=0 ; i<5 ; i++){
+        var p = document.createElement("p");
+        p.classList.add("mostsearch")
+        p.innerHTML=rsp.data[i];
+        contTrendings.append(p);
+     }
+
+})};
+
+//-------------------------------------------------------API CONNECTION GIFS TRENDIGS----------------------------------
+function gifTrendigs(){
+var urlGifTrendigs = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
+fetch (urlGifTrendigs)
+.then (r => r.json())
+.then ((rsp) => {
+    var contGif = document.getElementById("slider");
+    for (i=0 ; i<5 ; i++){
+        var img = document.createElement("img");
+        img.setAttribute("src",rsp.data[i].images.original.url);
+        img.classList.add("imgGif");
+        contGif.appendChild(img);
+    }
+})};
+
+async function search (busqueda){
+    var urlBusqueda =`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${busqueda}&limit=10`;
+    resp = await fetch(urlBusqueda);
+    info = await resp.json();
+
+    var contimages = document.getElementById("searchimagesresults");
+    console.log(info.data[1].images.original.url);
+    for (i=0 ; i<info.data.length ; i++){
+        var img = document.createElement("img");
+        img.setAttribute("src",info.data[i].images.original.url);
+        img.classList.add("imagesresults");
+        contimages.appendChild(img);
+    }
+
+
+    var contgeneral = document.getElementById("searchresults")
+    var txt = '<div class="borde"></div>' +
+    '<h1 class="titlesearch">'+busqueda+'</h1>'
+    contgeneral.insertAdjacentHTML('afterbegin',txt);
+}
+
+
+    var boton = document.getElementById("buttonsearch");
+    boton.addEventListener("click",clickbotonbusqueda);
+
+    function clickbotonbusqueda (){
+        var input = document.getElementById("search");
+        document.getElementById("title").style.display="none";
+        document.getElementById("busqueda").classList.add("activesearch");
+        boton.classList.add("icon-searchactive")
+        search(input.value);
+    }
+
+
+
+
+
+
+
+
+
+
+trendigs();
+gifTrendigs();
